95942383c451ab0c807c03804f62943e75456585
LeRatGondin <44407018+LeRatGondin@users.noreply.github.com>
2022-02-11 16:34:17 +0100

    Update Main.py

--- Xor/Main.py
+++ Xor/Main.py
@@ -22,107 +22,107 @@ from hashlib import sha256 , md5 , sha224 , sha512 , sha1
 import binascii
 import os
 def encode():
-	message = input("	Entrez le message a chiffrer : ")
-	key = input("	Entrez la clé : ")
+    message = input("   Entrez le message a chiffrer : ")
+    key = input("   Entrez la clé : ")
 
-	keys1 = str(sha256(key.encode ('ascii')).hexdigest())
-	keys2 = str(md5(key.encode ('ascii')).hexdigest())
-	keys3 = str(sha224(key.encode ('ascii')).hexdigest())
-	keys4 = str(sha512(key.encode ('ascii')).hexdigest())
-	keys5 = str(sha1(key.encode ('ascii')).hexdigest())
+    keys1 = str(sha256(key.encode ('ascii')).hexdigest())
+    keys2 = str(md5(key.encode ('ascii')).hexdigest())
+    keys3 = str(sha224(key.encode ('ascii')).hexdigest())
+    keys4 = str(sha512(key.encode ('ascii')).hexdigest())
+    keys5 = str(sha1(key.encode ('ascii')).hexdigest())
 
-	keys = keys1 + keys2 + keys3 + keys4 + keys5
-	keys = keys + keys4 + keys3 + keys2 +keys1 + keys
+    keys = keys1 + keys2 + keys3 + keys4 + keys5
+    keys = keys + keys4 + keys3 + keys2 +keys1 + keys
 
 
-	message_en_binaire = '0' + bin(int.from_bytes(message.encode(), 'big'))[2:]
-	keys_en_binaire = '0' + bin(int.from_bytes(keys.encode(), 'big'))[2:]
-	while len(message_en_binaire) >> len(keys_en_binaire):
-        	keys_en_binaire += keys_en_binaire
-	liste_message = list(message_en_binaire)
-	liste_key = list(keys_en_binaire)
+    message_en_binaire = '0' + bin(int.from_bytes(message.encode(), 'big'))[2:]
+    keys_en_binaire = '0' + bin(int.from_bytes(keys.encode(), 'big'))[2:]
+    while len(message_en_binaire) >> len(keys_en_binaire):
+            keys_en_binaire += keys_en_binaire
+    liste_message = list(message_en_binaire)
+    liste_key = list(keys_en_binaire)
 
-	longueur_message = len(message_en_binaire)
+    longueur_message = len(message_en_binaire)
 
-	try:
-		a = 0
-		resultat_binaire = ""
-		for loop in range(longueur_message):
-			if liste_key[a] == liste_message[a]:
-				resultat_binaire = resultat_binaire + "0"
-			else:
-				resultat_binaire = resultat_binaire + "1"
-			a = a + 1
+    try:
+        a = 0
+        resultat_binaire = ""
+        for loop in range(longueur_message):
+            if liste_key[a] == liste_message[a]:
+                resultat_binaire = resultat_binaire + "0"
+            else:
+                resultat_binaire = resultat_binaire + "1"
+            a = a + 1
 
-		result = resultat_binaire
-		print(f'	Voici votre message chiffré {result}')
-	except IndexError:
-		print("	Votre clé est trop petite pour votre message veuillez l'agrandir")
+        result = resultat_binaire
+        print(f'    Voici votre message chiffré {result}')
+    except IndexError:
+        print(" Votre clé est trop petite pour votre message veuillez l'agrandir")
 
 def decode():
-	message = input("	Entrez le message a dechiffrer : ")
-	key = input("	Entrez la clé : ")
+    message = input("   Entrez le message a dechiffrer : ")
+    key = input("   Entrez la clé : ")
 
-	keys1 = str(sha256(key.encode ('ascii')).hexdigest())
-	keys2 = str(md5(key.encode ('ascii')).hexdigest())
-	keys3 = str(sha224(key.encode ('ascii')).hexdigest())
-	keys4 = str(sha512(key.encode ('ascii')).hexdigest())
-	keys5 = str(sha1(key.encode ('ascii')).hexdigest())
+    keys1 = str(sha256(key.encode ('ascii')).hexdigest())
+    keys2 = str(md5(key.encode ('ascii')).hexdigest())
+    keys3 = str(sha224(key.encode ('ascii')).hexdigest())
+    keys4 = str(sha512(key.encode ('ascii')).hexdigest())
+    keys5 = str(sha1(key.encode ('ascii')).hexdigest())
 
-	keys = keys1 + keys2 + keys3 + keys4 + keys5
-	keys = keys + keys4 + keys3 + keys2 +keys1 + keys
+    keys = keys1 + keys2 + keys3 + keys4 + keys5
+    keys = keys + keys4 + keys3 + keys2 +keys1 + keys
 
-	message_en_binaire = message
-	keys_en_binaire = '0' + bin(int.from_bytes(keys.encode(), 'big'))[2:]
-	while len(message_en_binaire) >> len(keys_en_binaire):
-        	keys_en_binaire += keys_en_binaire
-	liste_message = list(message_en_binaire)
-	liste_key = list(keys_en_binaire)
+    message_en_binaire = message
+    keys_en_binaire = '0' + bin(int.from_bytes(keys.encode(), 'big'))[2:]
+    while len(message_en_binaire) >> len(keys_en_binaire):
+            keys_en_binaire += keys_en_binaire
+    liste_message = list(message_en_binaire)
+    liste_key = list(keys_en_binaire)
 
-	longueur_message = len(message_en_binaire)
+    longueur_message = len(message_en_binaire)
 
 
-	a = 0
-	resultat_binaire = ""
-	for loop in range(longueur_message):
-		if liste_key[a] == liste_message[a]:
-			resultat_binaire = resultat_binaire + "0"
-		else:
-			resultat_binaire = resultat_binaire + "1"
-		a = a + 1
-	try:
-		binary_int = int(resultat_binaire, 2)
-		byte_number = binary_int.bit_length() + 7 // 8
-		binary_array = binary_int.to_bytes(byte_number, "big")
-		result = binary_array.decode()
-		result_list = list(result)
-    		first_char = result_list[1]
-   		while result.startswith(first_char):
-        		result = result[1:]
-		print(f'	Voici votre message decodé : {result}')
-	except ValueError:
-		print('Vous devez entrer une clé et/ou un message valide')
-		pass
+    a = 0
+    resultat_binaire = ""
+    for loop in range(longueur_message):
+        if liste_key[a] == liste_message[a]:
+            resultat_binaire = resultat_binaire + "0"
+        else:
+            resultat_binaire = resultat_binaire + "1"
+        a = a + 1
+    try:
+        binary_int = int(resultat_binaire, 2)
+        byte_number = binary_int.bit_length() + 7 // 8
+        binary_array = binary_int.to_bytes(byte_number, "big")
+        result = binary_array.decode()
+        result_list = list(result)
+        first_char = result_list[1]
+        while result.startswith(first_char):
+            result = result[1:]
+        print(f'    Voici votre message decodé : {result}')
+    except ValueError:
+        print('Vous devez entrer une clé et/ou un message valide')
+        pass
 
-	
+    
 
 if __name__ == '__main__':
-	os.system('cls')
-	mode = input("""
-		__   __            ______                     _           
-		\ \ / /           |  ____|                   | |          
-		 \ V / ___  _ __  | |__   _ __   ___ ___   __| | ___ _ __ 
-		  > < / _ \| '__| |  __| | '_ \ / __/ _ \ / _` |/ _ \ '__|
-		 / . \ (_) | |    | |____| | | | (__ (_) | (_| |  __/ |   
-		/_/ \_\___/|_|    |______|_| |_|\___\___/ \__,_|\___|_|   
-		by LeRatGondin
+    os.system('cls')
+    mode = input("""
+        __   __            ______                     _           
+        \ \ / /           |  ____|                   | |          
+         \ V / ___  _ __  | |__   _ __   ___ ___   __| | ___ _ __ 
+          > < / _ \| '__| |  __| | '_ \ / __/ _ \ / _` |/ _ \ '__|
+         / . \ (_) | |    | |____| | | | (__ (_) | (_| |  __/ |   
+        /_/ \_\___/|_|    |______|_| |_|\___\___/ \__,_|\___|_|   
+        by LeRatGondin
 
-	Dechiffrer message : 1
-	Chiffrer message : 2
-	
-	>>> """)
-	if mode == "2":
-		encode()
-	if mode == "1":
-		decode()
-	input()
+    Dechiffrer message : 1
+    Chiffrer message : 2
+    
+    >>> """)
+    if mode == "2":
+        encode()
+    if mode == "1":
+        decode()
+    input()
